3672c1cc93e705255b9245b86c540feaa75b6608
Simeon Chakarov <simeon.chakarov@smarsh.com>
2023-05-03 10:11:34 +0300

    Create layout template for all of out pages and use jinja to extend the layout for the different pages

--- ChatTrack.py
+++ ChatTrack.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+dummyChats = [
+    {
+        'author': 'Jane Doe',
+        'channel': 'Teams',
+        'content': 'Jane: Hey, how are you? Client: Shut up and help me, lazy!',
+        'date_posted': 'May 2nd, 2023'
+    },
+    {
+        'author': 'Jane Doe',
+        'channel': 'Bloomberg chat',
+        'content': 'Jane: Hey, how can I help you? Client: Are you crazy!?',
+        'date_posted': 'May 2nd, 2023'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>ChatTrack is ready for work!</h1>"
+    return render_template('home.html', template="Home", chats=dummyChats)
 
 @app.route("/about")
 def about():
-    return "<p>ChatTrack is a SaaS that lets you monitor communications channels!</p>"
+    return render_template('about.html', template="About")
 
 if __name__ == '__main__':
     app.run(debug=True) # So changes get effect without restarting the app
